32dfaec696b8eb183d39faf16876cce0d252e9b2
Chris Lawlor <chris@pymetrics.com>
2021-01-12 12:48:42 -0500

    Initial commit

---
+++ README.rst
@@ -0,0 +1,61 @@
+================
+LD Feature Patch
+================
+
+
+.. image:: https://img.shields.io/pypi/v/ld_patch.svg
+        :target: https://pypi.python.org/pypi/ld_patch
+
+.. image:: https://img.shields.io/travis/pymetrics/ld_patch.svg
+        :target: https://travis-ci.com/pymetrics/ld_patch
+
+.. image:: https://readthedocs.org/projects/ld-patch/badge/?version=latest
+        :target: https://ld-patch.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+
+
+Patch `Launch Darkly`_ feature flags for unit testing
+
+
+* Free software: Apache Software License 2.0
+* Documentation: https://ld-patch.readthedocs.io.
+
+
+Features
+--------
+
+.. code:: python
+
+    from ld_patch import patch_feature
+    from unittest import TestCase
+
+    class MyTestCase(TestCase):
+
+    @patch_feature("my.flag.key", True)
+    def test_my_code(self):
+
+        # Feature defaults to False
+        show_feature = ldclient.variation(
+            "my.flag.key",
+            {"key": "test@example.com":},
+            False
+        )
+
+        # But patch_feature set it to True
+        assert show_feature == True
+
+Installation
+
+.. code:: bash
+
+    python -m pip install ld-feature-patch
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
+.. _`Launch Darkly`: https://launchdarkly.com/
---
+++ ld_patch/ld_patch.py
@@ -0,0 +1,64 @@
+import ldclient
+from unittest.mock import MagicMock
+from functools import wraps
+from typing import Any
+
+
+TEST_PREFIX = "test_"
+
+
+class patch_feature:
+    def __init__(self, feature_flag: str, value: Any, client=None):
+        self.feature_flag = feature_flag
+        self.value = value
+        self.client = client or ldclient.get()
+        self.original_variation = self.client.variation
+
+    def copy(self):
+        return patch_feature(
+            feature_flag=self.feature_flag, value=self.value, client=self.client
+        )
+
+    def decorate_class(self, klass):
+        # based on unittest.mock._patch.decorate_class
+        for attr in dir(klass):
+            if not attr.startswith(TEST_PREFIX):
+                continue
+
+            attr_value = getattr(klass, attr)
+            if not hasattr(attr_value, "__call__"):
+                continue
+            print(f"PATCHING {klass.__name__}.{attr}")
+            patcher = self.copy()
+            setattr(klass, attr, patcher(attr_value))
+        return klass
+
+    def decorate_method(self, method):
+        @wraps(method)
+        def wrapper(*args, **kwargs):
+            with patch_feature(self.feature_flag, self.value):
+                return method(*args, **kwargs)
+
+        return wrapper
+
+    def __enter__(self):
+        def get_variation(flag, user, default):
+            if flag == self.feature_flag:
+                return self.value
+            return self.original_variation(flag, user, default)
+
+        variation = MagicMock(spec=self.original_variation)
+        variation.side_effect = get_variation
+        self.client.variation = variation
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.client.variation = self.original_variation
+
+    def __call__(self, thing):
+
+        if isinstance(thing, type):
+            ret = self.decorate_class(thing)
+        else:
+            ret = self.decorate_method(thing)
+
+        return ret
